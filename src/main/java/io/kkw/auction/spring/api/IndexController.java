484bfa584eecdc4e490ccbffa1970d9cc40b2216
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-06-03 21:39:21 +0900

    201906032139

--- src/main/java/io/kkw/auction/spring/api/IndexController.java
+++ src/main/java/io/kkw/auction/spring/api/IndexController.java
@@ -4,12 +4,17 @@ package io.kkw.auction.spring.api;
 import io.kkw.auction.spring.bean.AucProgress;
 import io.kkw.auction.spring.service.AuctionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+import java.util.ResourceBundle;
 
 @Controller
 @RequestMapping("/")
@@ -23,17 +28,26 @@ public class IndexController {
     public String index(Model model) {
         return "index";
     }
-    public static boolean time=false;
+
+    public static Date date = new Date();
 
 
     //시간 지난거 확인
     @PostMapping("/check")
     @ResponseBody
-    public String time(){
-        time = true;
-        auctionService.findCheck();
-        time = false;
-        return null;
+    public ResponseEntity<Object> time(){
+        Date now = new Date();
+        if (date.getTime() - now.getTime() >= 60000) {
+            System.out.println("Check Start");
+            long start = System.currentTimeMillis();
+            auctionService.findCheck();
+            long end = System.currentTimeMillis();
+            date = now;
+            System.out.println("Check End"+(end-start)/1000.0);
+            return new ResponseEntity<>(true,HttpStatus.OK);
+        }else
+            return new ResponseEntity<>(false, HttpStatus.OK);
+
     }
 
 
